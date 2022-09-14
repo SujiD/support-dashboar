ea24a762f9950190259b4f5327a0b0b79dd66e81
Sujith <sujuth.dachepally@marklogic.com>
2022-09-14 19:33:57 +1000

    changes

--- src/components/calendar/Calendar.js
+++ src/components/calendar/Calendar.js
@@ -53,7 +53,6 @@ const Calendar = () => {
           max={yy + "-" + String(today.getMonth() + 1).padStart(2, "0") + "-" + String(today.getDate()).padStart(2, "0")}
         />
       </div>
-      {/* ) : null} */}
     </div>
   );
 };
