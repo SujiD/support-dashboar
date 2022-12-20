81b494461a8fbbcaa96347748571d17315f8a370
SujithDach <Sujith.Dachepally@student.uts.edu.au>
2022-12-20 18:05:01 +1100

    Working on fixing issues

--- src/components/LandingPage.jsx
+++ src/components/LandingPage.jsx
@@ -1,9 +1,9 @@
 import { Button } from "react-bootstrap";
 import { useNavigate } from "react-router-dom";
 
-const LoginButton = () => {
+const LandingPage = () => {
   const navigate = useNavigate();
   return <Button onClick={() => navigate("/authenticate")}>Login</Button>;
 };
 
-export default LoginButton;
+export default LandingPage;
